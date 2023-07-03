48232ad383346efc9ed7ffec7f4739215f6cb7db
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2023-07-03 13:48:05 +0300

    perf(comment): remove heavy for performance queries

--- queries/comment/highlights.scm
+++ queries/comment/highlights.scm
@@ -4,17 +4,11 @@
   ":" @punctuation.delimiter)
   (#any-of? @text.todo "TODO" "WIP"))
 
-("text" @text.todo @nospell
- (#any-of? @text.todo "TODO" "WIP"))
-
 ((tag
   (name) @text.note @nospell
   ("(" @punctuation.bracket (user) @constant ")" @punctuation.bracket)?
   ":" @punctuation.delimiter)
   (#any-of? @text.note "NOTE" "XXX" "INFO" "DOCS" "PERF" "TEST"))
-
-("text" @text.note @nospell
- (#any-of? @text.note "NOTE" "XXX" "INFO" "DOCS" "PERF" "TEST"))
 
 ((tag
   (name) @text.warning @nospell
@@ -22,20 +16,10 @@
   ":" @punctuation.delimiter)
   (#any-of? @text.warning "HACK" "WARNING" "WARN" "FIX"))
 
-("text" @text.warning @nospell
- (#any-of? @text.warning "HACK" "WARNING" "WARN" "FIX"))
-
 ((tag
   (name) @text.danger @nospell
   ("(" @punctuation.bracket (user) @constant ")" @punctuation.bracket)?
   ":" @punctuation.delimiter)
   (#any-of? @text.danger "FIXME" "BUG" "ERROR"))
-
-("text" @text.danger @nospell
- (#any-of? @text.danger "FIXME" "BUG" "ERROR"))
-
-; Issue number (#123)
-("text" @number
- (#lua-match? @number "^#[0-9]+$"))
 
 ((uri) @text.uri @nospell)
